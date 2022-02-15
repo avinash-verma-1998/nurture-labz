7b6e907064bcc494e79ba33d6810627430a5c5d4
some <some@some.com>
2022-02-15 15:57:33 +0530

    added fonts

---
+++ src/Nav/Nav.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar (){
+
+    return (
+        <nav className="nav">Lorem Ipsum is simply dummy text of the printing
+        </nav>
+    );
+}
+
+export default Navbar;
